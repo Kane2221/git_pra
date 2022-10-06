eca82d215df531ab429dbeccd0e994f6b6e86931
Neil <tasumi@gmail.com>
2022-10-06 13:59:13 +0800

    no message

--- git_pra/Program.cs
+++ git_pra/Program.cs
@@ -12,6 +12,11 @@ namespace git_pra
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+        }
+        public static string Program1(string name)  
+        {
+           
         }
     }
 }
