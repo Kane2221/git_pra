5493f47c6dfae856c54400132b11ffc062ef5a8b
Senny <senny8617@gmail.com>
2022-10-06 16:24:45 +0800

    123123

---
+++ git_pra/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_pra
+{
+    internal class Class1
+    {
+    }
+}
